15d6e94b7140321a578c0361b6c12ecc266ef03d
Иншаков Сергей <inshakovsa@edu.mos.ru>
2020-02-07 09:56:52 +0300

    создал interface для оффера

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Offer} from '../shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
@@ -7,11 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  offers: string[] = [
-    `Beautiful &amp; luxurious apartment at great location`,
-    `Wood and stone place`,
-    `Canal View Prinsengracht`,
-    `Nice, cozy, warm big bed apartment`
+  offers: Offer[] = [
+    {title: `Beautiful &amp; luxurious apartment at great location`},
+    {title: `Wood and stone place`},
+    {title: `Canal View Prinsengracht`},
+    {title: `Nice, cozy, warm big bed apartment`}
   ];
 
   constructor() {
--- src/app/shared/components/offer/offer.component.ts
+++ src/app/shared/components/offer/offer.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Offer} from '../../interfaces';
 
 @Component({
   selector: 'app-offer',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class OfferComponent implements OnInit {
 
-  @Input() offer: string;
+  @Input() offer: Offer;
 
   constructor() { }
 
