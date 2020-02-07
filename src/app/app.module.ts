4f4f3c93679f3b81ad2c71f084364b11efd812a8
Иншаков Сергей <inshakovsa@edu.mos.ru>
2020-02-07 09:44:23 +0300

    создал компонент оффера; прокинул в него данные

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { OfferComponent } from './shared/components/offer/offer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,
