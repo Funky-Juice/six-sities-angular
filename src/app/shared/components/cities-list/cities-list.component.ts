6dcb53a8af6a4f19fc693e4786d07805eaae86bf
Funky-Juice <isa86box@gmail.com>
2020-02-08 18:14:05 +0300

    рендер списка городов на основе динамических данных

--- src/app/shared/components/cities-list/cities-list.component.ts
+++ src/app/shared/components/cities-list/cities-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Offer} from '../../interfaces';
 
 @Component({
   selector: 'app-cities-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesListComponent implements OnInit {
 
+  @Input() offers: Offer[];
+
+  activeItem = 0;
+  filteredCitiesList = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.filterCities(this.offers);
+    this.setRandomActiveItem();
+  }
+
+  filterCities(offers: Offer[]) {
+    this.filteredCitiesList = [...new Set(
+      offers.map((offer) => JSON.stringify(offer.city))
+    )].map((it) => JSON.parse(it));
+  }
+
+  setRandomActiveItem() {
+    this.activeItem = Math.floor(Math.random() * Math.floor(this.filteredCitiesList.length));
+  }
+
+  setActiveItem(index: number) {
+    this.activeItem = index;
   }
 
 }
