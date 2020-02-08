49e0248af7c79d2006e64bd2eb7b176c89fb09f1
Funky-Juice <isa86box@gmail.com>
2020-02-08 15:24:48 +0300

    вывод на карту маркеров офферов

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import {AfterContentInit, Component, OnInit} from '@angular/core';
 import * as Leaflet from 'leaflet';
+import {offers} from '../../../../assets/mocks/offers';
 
 @Component({
   selector: 'app-map',
@@ -9,8 +10,12 @@ import * as Leaflet from 'leaflet';
 export class MapComponent implements OnInit, AfterContentInit {
 
   map = null;
-  city = [52.38333, 4.9];
   zoom = 12;
+  city = [52.38333, 4.9];
+  iconSize = [30, 30];
+  iconUrl = `assets/img/pin.svg`;
+  markers = Leaflet.layerGroup();
+  activeMarker = Leaflet.layerGroup();
 
   constructor() { }
 
@@ -19,6 +24,14 @@ export class MapComponent implements OnInit, AfterContentInit {
 
   ngAfterContentInit() {
     this._mapInit('map-id');
+    this._renderPoints(offers);
+  }
+
+  get icon() {
+    return Leaflet.icon({
+      iconUrl: this.iconUrl,
+      iconSize: this.iconSize
+    });
   }
 
   _mapInit(container) {
@@ -41,6 +54,21 @@ export class MapComponent implements OnInit, AfterContentInit {
         <a href="https://carto.com/attributions">CARTO</a>`
       })
       .addTo(this.map);
+  }
+
+  _renderPoints(offersList) {
+    this.markers.clearLayers();
+    this.activeMarker.clearLayers();
+
+    offersList.map((offer) => {
+      const {latitude: x, longitude: y} = offer.location;
+      const marker = Leaflet.marker(
+        [x, y], {icon: this.icon}
+      );
+      this.markers.addLayer(marker);
+    });
+
+    this.map.addLayer(this.markers);
   }
 
 }
